c8dd610c57dd05a740039a755dd3b4e10d2519cd
ajaymotupalli <ajaymotupalli4444@gmail.com>
2024-12-08 15:34:16 +0530

    saving data

--- Joints/innerJoin.sql
+++ Joints/innerJoin.sql
@@ -2,4 +2,4 @@ select *
 from call_center
     inner join catalog_returns
     on call_center.cc_call_center_sk = catalog_returns.cr_call_center_sk
-limit 100;
+-- limit 100;
--- Joints/rightJoin.sql
+++ Joints/rightJoin.sql
@@ -2,4 +2,4 @@ select call_center.cc_call_center_sk, catalog_sales.*
 from call_center
     right join catalog_SALES
     on call_center.cc_call_center_sk = catalog_sales.cs_call_center_sk
-limit 100;
+-- limit 100;
